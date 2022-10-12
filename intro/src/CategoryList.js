dec0451d9898c13a65e8cb08439720a7c66b22ea
Ali Durna <84575898+ALDOR99@users.noreply.github.com>
2022-10-12 16:10:32 +0300

    Revert ".1"

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react'
-import {ListGroup,ListGroupItem} from "reactstrap" //import
 
 export default class CategoryList extends Component {
   render() {
     return (
-        
-        <div>
-            <h3>Category List</h3>
-            <ListGroup>
-            <ListGroupItem>Cras justo odio</ListGroupItem>
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Morbi leo risus</ListGroupItem>
-            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-        </div>
+        <h3>Category List</h3>
     )
   }
 }
